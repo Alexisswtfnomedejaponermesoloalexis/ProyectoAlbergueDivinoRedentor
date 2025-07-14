3a7b73ec3494e5b0c05ffe4132fcec7fa4f5bb17
bluegglweed <bluegglweed@gmail.com~>
2025-07-13 22:35:16 -0600

    Alexis / urls edit y forms

--- farmacia/urls.py
+++ farmacia/urls.py
@@ -9,4 +9,14 @@ urlpatterns = [
     path('salidas/', views.salidas, name='salidas'),
     path('expediente/<int:id>/', views.expediente, name='expediente'),
     path('reportes/', views.reportes, name='reportes'),
+
+    #forms
+
+    path('medicamentos/nuevo/', views.medicamento_nuevo, name='medicamento_nuevo'),
+    path('medicamentos/editar/<int:id>/', views.medicamento_editar, name='medicamento_editar'),
+    path('salidas/nueva/', views.salida_nueva, name='salida_nueva'),
+    path('pacientes/nuevo/', views.paciente_nuevo, name='paciente_nuevo'),
+    path('pacientes/editar/<int:id>/', views.paciente_editar, name='paciente_editar'),
+    path('medicos/nuevo/', views.medico_nuevo, name='medico_nuevo'),
+    path('medicos/editar/<int:id>/', views.medico_editar, name='medico_editar'),
 ]
