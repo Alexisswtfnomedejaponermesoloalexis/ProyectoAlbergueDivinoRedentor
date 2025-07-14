99281d5800a40715449ce4a43c63adbcb7e891c9
Alanblhs <alanblhs@gmail.com>
2025-07-13 20:02:59 -0600

    Alan/Modelo CategoriaMedicamento

--- farmacia/models.py
+++ farmacia/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class CategoriaMedicamento(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = "Categoría de Medicamento"
+        verbose_name_plural = "Categorías de Medicamentos"
+
+    def _str_(self):
+        return self.nombre
