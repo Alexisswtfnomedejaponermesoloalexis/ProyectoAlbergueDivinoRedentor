5cb11712106bb2798071249356da0d9e4a027806
Your Name <macielbrayn@gmail.com>
2025-07-13 21:29:20 -0600

    Gerardo/App.py

--- farmacia/apps.py
+++ farmacia/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class FarmaciaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'farmacia'
+    verbose_name = "Sistema Farmacéutico"
