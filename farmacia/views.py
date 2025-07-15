1a641b219e648a0ba75454c78be9e3531d35ef1b
bluegglweed <bluegglweed@gmail.com~>
2025-07-15 12:57:21 -0600

    ALEXIS / ALTER COMMITS

--- farmacia/views.py
+++ farmacia/views.py
@@ -26,6 +26,9 @@ from django.db.models import Sum
 from .models import CategoriaMedicamento, Medicamento, Paciente, ExpedienteMedico, SalidaMedicamento, Medico
 from .forms import MedicamentoForm, PacienteForm, SalidaMedicamentoForm, HistoriaMedicaForm, MedicoForm
 
+#FUNCIÓN DEL MENÚ PRINCIPAL, LA FUNCIÓN INDEX PERMITE MOSTRAR UNA INTERFAZ MÁS INTUITIVA MOSTRANDO CARDS DE INFORMACIÓN.
+# PERMITE MOSTRAR LOS EL CONTEO D EMÉDICOS, PACIENTES, LOS MEDICAMENTOS_CRITICOS
+
 def index(request):
     context = {
         'criticos_count': Medicamento.objects.filter(cantidad__lt=5).count(),
@@ -36,6 +39,10 @@ def index(request):
     }
     
     return render(request, 'index.html', context)
+
+# FUNCIÓN PACIENTES, DICHA FUNCIÓN ALMACENA A LOS PACIENTES REGISTRADOS EN 'pacientes_list' ASIGNANDOLE LOS OBJETOS DADOS DE ALTA EN REFERENCIA AL MODELO 'Paciente'
+# AL ÚTLIMO RETORNA LOS DATOS ALMACENADOS A SU RESPECTIVA VISTA (EN ESTE CASO ES "pacientes.html")
+
 def pacientes(request):
     pacientes_list = Paciente.objects.all()
     context = {
@@ -43,12 +50,19 @@ def pacientes(request):
     }
     return render(request, 'pacientes.html', context)
 
+# FUNCIÓN MÉDICOS, ESTA FUNCIÓN SIRVE PARA ALMACENAR A LOS MÉDICOS REGISTRADOS EN "medicos_list" ASIGNANDOLE LOS OBJETOS DADOS DE ALTA EN REFERENCIA AL MODELO 'médico
+# AL ÚTLIMO RETORNA LOS DATOS ALMACENADOS A SU RESPECTIVA VISTA (EN ESTE CASO ES "medicos.html")
+
+
 def medicos(request):
     medicos_list = Medico.objects.all()
     context = {
         'medicos': medicos_list
     }
     return render(request, 'medicos.html', context)
+
+# FUNCIÓN MEDICAMENTOS, AQUÍ SE REALIZA LOS EDITS PARA CONFIGURAR SOBRE LOS MEDICAMENTOS QUE SE ENCUENTRAN EN INVENTARIO CRÍTICO, MEDIANTE EL (.filter(cantidad__5))
+# AL IGUAL QUE SE SELECCIONA LA CATEGORIA (DICHAS CATEGORIAS SE ASIGNAN DIRECTAMENTE EN EL PANEL DEL ADMINISTRADOR)
 
 def medicamentos(request):
     medicamentos_list = Medicamento.objects.select_related('categoria').all()
@@ -62,6 +76,9 @@ def medicamentos(request):
     }
     return render(request, 'medicamentos.html', context)
 
+# FUNCIÓN DE SALIDAS, AQUÍ SE RELACIONA CON EL MODELO 'SalidaMedicamento' 
+# AQUÍ MUESTRA LAS SALIDAS REGISTRADAS CON ANTERIORIRDAD, MOSTRANDO EL MÉDICO, PACIENTE Y MEDICAMENTO ASIGNADO
+
 def salidas(request):
     salidas_list = SalidaMedicamento.objects.select_related('medicamento', 'paciente', 'medico').all()
     context = {
@@ -69,7 +86,11 @@ def salidas(request):
     }
     return render(request, 'salidas.html', context)
 
-
+# FUNCIÓN DE REPORTES, *MÓDUCLO AÚN SIN TÉRMINAR*
+# AQUÍ HACEMOS USO DEL MÉTODO .ANNOTATE()
+# DICHO MÉTODO SE UTILIZA PARA REALIZAR AGREGACIONES Y CÁLCULOS SOBRE GRUPOS DE REGISTROS EN ESTE CASO A 'cantidad'
+# SE REQUIERE IMPORTAR 'SUM'
+# EL MÉTODO DE ORDER_BY SIRVE PARA ORDENAR LOS VALORES
 def reportes(request):
     # Estadísticas de medicamentos más entregados
     medicamentos_mas_entregados = SalidaMedicamento.objects.values(
@@ -78,14 +99,21 @@ def reportes(request):
         total=Sum('cantidad')
     ).order_by('-total')[:5]
     
-    # Medicamentos críticos
+    # AQUÍ ALMACENA LOS MEDICAMENTOS CRÍTICOS FILTRANDO SOLO LA CANTIDAD DE EXISTENCIAS __LT5
     medicamentos_criticos = Medicamento.objects.filter(cantidad__lt=5)
     
+    ## MÓDULO DE REPORTES AÚN SIN TERMINAR
+
     context = {
         'medicamentos_mas_entregados': medicamentos_mas_entregados,
         'medicamentos_criticos': medicamentos_criticos
     }
     return render(request, 'reportes.html', context)
+
+
+# FUNCIÓN DE EXPEDIENTES, DICHA FUNCIÓN DE EXPEDIENTE TRABAJA EN CONJUNTO CON 3 MODELOS, EXTRAE TODOS LOS DATOS DEL PACIENTE
+# UTILIZA EL MODELO HISTOPRIA_MÉDICA, HISTORIA_MÉDICA FUNCIONA COMO NOTAS QUE AGREGA EL MÉDICO HACÍA EL PACIENTE
+#  AL IGUAL QUE VÍNCULA LAS SALIDAS DE MEDICAMENTO QUE SE LE HAN ASIGNADO AL PACIENTE 
 
 
 def expediente(request, id):
@@ -94,6 +122,7 @@ def expediente(request, id):
     salidas = SalidaMedicamento.objects.filter(paciente=paciente).select_related('medicamento')
     
     # Obtener todas las historias médicas ordenadas por fecha descendente
+    # HACEMOS USO DEL MÉTODO ==POST
     historias = expediente.historias.all().order_by('-fecha_creacion')
     
     if request.method == 'POST':
@@ -117,15 +146,24 @@ def expediente(request, id):
     return render(request, 'expediente.html', context)
 
 
-##FORMS 
+## F    O   R   M   S        AQUÍ SE ALMACENAN LAS FUNCIONES DE LOS FORMULARIOS DE AGREGAR UNA ALTA O EDITAR UNA ALTA REGISTRADA        ##
+
+
+# FUNCIÓN MEDICAMENTO_NUEVO CUYA FUNCIÓN SIRVE PARA DAR DE ALTA MEDICAMENTOS
+# HACEMOS USO DEL MÉTODO POST PARA HACE ENVÍO DE DATOS DEL FORMULARIO 
 
 def medicamento_nuevo(request):
     if request.method == 'POST':
+        # (request.POST) FORMULARIO CON DATOS RECIBIDOS
         form = MedicamentoForm(request.POST)
+        # SE VALIDAN LOS DATOS
         if form.is_valid():
+            #LOS GUARDA EN LA BDD
             form.save()
             messages.success(request, 'Medicamento creado correctamente')
             return redirect('medicamentos')
+        
+        #AQUÍ SE SOLICITA EL FORMULARIO VACÍO
     else:
         form = MedicamentoForm()
     
@@ -135,16 +173,25 @@ def medicamento_nuevo(request):
     }
     return render(request, 'forms/medicamento_form.html', context)
 
+# FUNCIÓN MEDICAMENTO_EDITAR, DICHA FUNCIÓN SIRVE PARA EDITAR LOS MEDCICAMENTOS REGISTRADOS
+# HACEMOS USO DEL ('get_object_or_404' por si no identifica el medicamento, e sporque no esta registrado aún, o en sud efecto hay un error)
+# SE IDENTIFICA MEDIANTE LA PK=PRIMARY KEY QUE EN ESTE CASO ES EL ID DEL MEDICAMENTO
+
 def medicamento_editar(request, id):
     medicamento = get_object_or_404(Medicamento, pk=id)
     
+    #AQUÍ SE ENVÍA LOS DATOS EDITADOS
     if request.method == 'POST':
+        #FORMULARIO DE DATOS (MEDICAMENTO FORM) CON LA INSTANCIA EXISTENTE (OBJ.MEDICAMENTO)
         form = MedicamentoForm(request.POST, instance=medicamento)
         if form.is_valid():
+            # ACTUALIZA EL REGISTRO EXISTENTE
             form.save()
             messages.success(request, 'Medicamento actualizado correctamente')
             return redirect('medicamentos')
+        # SOLICITUD PARA EDITAR
     else:
+        #SE PRECARGA EL FORMULARIO CON LOS DATOS EXISTENTES
         form = MedicamentoForm(instance=medicamento)
     
     context = {
