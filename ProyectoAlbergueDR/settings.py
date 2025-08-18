5b2f437fea4218e27b8d6fac69f375c299614764
Alexisswtfnomedejaponermesoloalexis <bluegglweed@gmail.com>
2025-08-18 17:10:11 +0000

    Cambios hechos en Files: actualización de templates y ajustes

--- ProyectoAlbergueDR/settings.py
+++ ProyectoAlbergueDR/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,7 +56,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ProyectoAlbergueDR.urls'
 
-import os
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -81,14 +81,32 @@ WSGI_APPLICATION = 'ProyectoAlbergueDR.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 
-    
+
+}
+"""
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AlbergueDivinoRe$default',
+        'USER': 'AlbergueDivinoRe',
+        'PASSWORD': 'Alexsensei777',
+        'HOST': 'AlbergueDivinoRedentor.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
+    }
 }
 
 
